287aa19497da37f1d1caabbefdb7a8c5f53efc20
IanDAW2 <aaaa@aaabbb.com>
2023-11-15 09:48:53 +0100

    Ejercicio de Cifrado

---
+++ Ejercicios/CifradoPE/cifrado.js
@@ -0,0 +1,34 @@
+// const regex = "/^[0-9\u00E0\u00FCs]+$/"
+// const codificacion = "pe"
+const vocales = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
+function cifradoPe(palabra) {
+  arrayPalabra = [...palabra]
+  let resultado = ""
+  console.log(arrayPalabra)
+  arrayPalabra.forEach((letra, i) => {
+    if (
+      vocales.includes(letra) &&
+      arrayPalabra[i + 1] &&
+      !vocales.includes(arrayPalabra[i + 1])
+    ) {
+      resultado += letra + "pe"
+    } else {
+      resultado += letra
+    }
+  })
+  return "pe" + resultado
+}
+
+console.log(cifradoPe("Autenticacion"))
+
+/**
+ * Escribe una funcion que acepte una unica palabra (no hay espacios en blanco ni tildes) y la devuelva codificada siguiendo las siguientes reglas:
+ * * La palabra codificada debe comenzar con "pe".
+ * * La "pe" va detras de una vocal.
+ * * Cuando hay varias vocales juntas, la "pe" va solo despues de la ultima vocal.
+ * * Cuando la palabra termina con una vocal, no se agrega el "pe" despues.
+ * * La cadena de palabras se compone unicamente de caracteres en minusculas y mayusculas.
+ *
+ * Obtener el cifrado de la palabra *Autenticacion* y colocarlo en una linea de comentario, al final del codigo.
+ */
