b60c9968ce76b24de6ea55bfd6a596acfbcf0e47
IanDAW2 <aaaa@aaabbb.com>
2023-12-04 12:10:25 +0100

    Clase 04-12 Contador de Clicks

---
+++ Clase/Diciembre/Clase0412/contadorClicks.js
@@ -0,0 +1,22 @@
+const boton = document.getElementById('contador')
+// const menosBoton = document.getElementById('restarContador')
+const parrafo = document.getElementById('clicks')
+var contador = 0
+
+boton.addEventListener('click', contadorSumaResta)
+boton.addEventListener('auxclick', contadorSumaResta)
+document.oncontextmenu = () => false
+// menosBoton.addEventListener('click', contadorSumaResta, false)
+
+function contadorSumaResta(e) {
+  switch (e.type) {
+    case 'click':
+      contador++
+      parrafo.textContent = contador
+      break
+    case 'auxclick':
+      contador--
+      parrafo.textContent = contador
+      break
+  }
+}
