607ee5d199bb9c08bf4bbb17ea574106a7ac46f7
IanDAW2 <aaaa@aaabbb.com>
2023-12-20 10:46:03 +0100

    Inicio Buscaminas

--- Ejercicios/Buscaminas/iniciarBuscaminas.js
+++ Ejercicios/Buscaminas/iniciarBuscaminas.js
@@ -1,12 +1,26 @@
+const formValores = document.getElementById('formValor')
+const inputAncho = document.getElementById('ancho')
+const inputAlto = document.getElementById('alto')
+const tabla = document.getElementById('tabla')
+
 function generarTableroJS() {
   // proximamente
+  formValores.addEventListener('submit', event => {
+    event.preventDefault()
+
+    let ancho = botonAncho.value
+    let alto = botonAlto.value
+  })
 }
+
 function numeroAleatorio() {
   // proximamente
 }
+
 function colocarBombasTableroJS() {
   // proximamente
 }
+
 function dibujarTableroHTML() {
   // proximamente
 }
