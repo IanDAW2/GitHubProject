d22b35502adac87fa03c227b6e9a84eeb57d943c
IanDAW2 <micursodawbarajas@gmail.com>
2024-01-17 13:33:35 +0100

    Buscaminas Primer Avance

--- Ejercicios/Buscaminas/iniciarBuscaminas.js
+++ Ejercicios/Buscaminas/iniciarBuscaminas.js
@@ -1,26 +1,59 @@
-const formValores = document.getElementById('formValor')
-const inputAncho = document.getElementById('ancho')
-const inputAlto = document.getElementById('alto')
 const tabla = document.getElementById('tabla')
 
-function generarTableroJS() {
-  // proximamente
-  formValores.addEventListener('submit', event => {
-    event.preventDefault()
+const botonFacil = document.getElementById('nFacil')
+botonFacil.addEventListener('click', generarTableroJS)
 
-    let ancho = botonAncho.value
-    let alto = botonAlto.value
-  })
+const botonIntermedio = document.getElementById('nIntermedio')
+botonIntermedio.addEventListener('click', generarTableroJS)
+
+const botonDificil = document.getElementById('nDificil')
+botonDificil.addEventListener('click', generarTableroJS)
+
+const matriz = []
+
+function generarTableroJS(event) {
+  switch (event.target) {
+    case botonFacil: // En el modo facil tendremos una tabla de 8x8
+      tabla.style.cssText += `grid-template-rows: repeat(8, 1fr);
+      grid-template-columns: repeat(8, 1fr);`
+      for (let index = 0; index < 8; index++) matriz.push([])
+      dibujarTableroHTML(8 * 8)
+      break
+    case botonIntermedio: // En el modo intermedio tendremos una tabla de 16x16
+      tabla.style.cssText += `grid-template-rows: repeat(16, 1fr);
+    grid-template-columns: repeat(16, 1fr);`
+      for (let index = 0; index < 16; index++) matriz.push([])
+      dibujarTableroHTML(16 * 16)
+      break
+    case botonDificil: // En el modo dificil tendremos una tabla de 30x16
+      tabla.style.cssText += `grid-template-columns: repeat(30, 1fr);
+    grid-template-rows: repeat(16, 1fr);`
+      for (let index = 0; index < 16; index++) matriz.push([])
+      dibujarTableroHTML(30 * 16)
+      break
+  }
 }
 
 function numeroAleatorio() {
   // proximamente
 }
 
-function colocarBombasTableroJS() {
+function colocarBombasTableroJS(numBombas) {
   // proximamente
+  Array.from(tabla.querySelectorAll('button')).forEach(bt => {
+    if (Math.floor(Math.random * 15) === 1) {
+      // Tiene una probabilidad de 15% de que sea bomba
+      bt.style.cssText += `background-image: url('assets/mine-50.png');
+      background-size: cover;`
+      bt.id = 1
+      if (--numBombas == 0) return // Si se acaban el numero de bombas totales por dificultad el resto no tienen
+    }
+  })
 }
 
-function dibujarTableroHTML() {
-  // proximamente
+function dibujarTableroHTML(numBT) {
+  for (let index = 0; index < numBT; index++) {
+    tBoton = document.createElement('button')
+    tabla.append(tBoton)
+  }
 }
