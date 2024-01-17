8d8a3efd99ae14707d502ce5b0c7e0b998805e46
IanDAW2 <micursodawbarajas@gmail.com>
2024-01-17 13:33:19 +0100

    Clase 17-01 DOM, preventDefault, form...

---
+++ Ejercicios/DOM/listaCompra/listaCompra.js
@@ -0,0 +1,25 @@
+const lista = document.getElementById('lista')
+const formLista = document.getElementById('formLista')
+
+formLista.addEventListener('submit', e => {
+  e.preventDefault()
+  const li = document.createElement('li')
+  const input = formLista.querySelector('input')
+  if (input.value) {
+    li.innerText = input.value
+    input.value = null
+
+    li.addEventListener('click', () => li.remove())
+
+    lista.appendChild(li)
+    ordenarLista()
+  }
+})
+
+function ordenarLista() {
+  const nuevaLista = Array.from(lista.children).sort((a, b) =>
+    a.innerText.localeCompare(b.innerText)
+  )
+  lista.innerHTML = null
+  nuevaLista.forEach(li => lista.appendChild(li))
+}
