6234bfd8b4159ea546cc7b00a8bc957c9ddd7ca9
IanDAW2 <ian.burford@educa.madrid.org>
2024-02-13 19:05:47 +0100

    Import, Modulos Clase

---
+++ Clase/Febrero/Modulos/funciones.js
@@ -0,0 +1,10 @@
+// Exportar un unico elemento
+export const cadenaMayusculas = str => str.toUpperCase()
+
+const NUMEROPI = Math.PI
+const NUMEROE = Math.E
+const NUMEROCERO = 0.0
+
+// Esportar mas de un elemento simultaneamente
+
+export { NUMEROPI, NUMEROE, NUMEROCERO }
---
+++ Clase/Febrero/Modulos/modulos.js
@@ -0,0 +1,9 @@
+import { cadenaMayusculas } from './funciones.js'
+
+const saludo = cadenaMayusculas('!hola, bienvenido a mi modulo!')
+
+console.log(saludo) // !HOLA, BIENVENIDO A MI MODULO!
+
+import {NUMEROCERO, NUMEROE, NUMEROPI} from './funciones.js';
+
+console.log(NUMEROCERO, NUMEROE, NUMEROPI);
